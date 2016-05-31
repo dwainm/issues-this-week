46e387c5b95d2e5c8339c0b8f0ac4efd63166c03
Dwain Maralack <dwain.maralack@gmail.com>
2016-05-31 23:28:16 +0200

    text area with submission handling initial code

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,43 @@
-var firebaseConnection = new Firebase("https://wcbookings-1253.firebaseio.com/");
+var restDb = new Firebase("https://wcbookings-1253.firebaseio.com/");
+var issuesRef = restDb.child("issues");
 
 var IssuesApp = React.createClass({
 
 	handleSubmit: function( e ){
-		
+		e.preventDefault();
 		console.log('submitted');
+
+		 var usernameInput = document.querySelector('#issue-input');
+		 var issueList     = 
+		 usernameInput.value.split('\n');
+		 //usernameInput.value = '';	
+		
+		 this.storeIssues();
+		
+	},
+
+	storeIssues: function( issueList ){
+		
+		issueList.forEach( function(issueUrl, index ){
+
+			var today = new Date();
+			issue = { url: issueUrl, author: '', lables:'', created: today.toJSON  };
+			restDb.issuesRef(  )	
+
+		});
+issues
+- id
+- url
+- author
+- lables
+- date created
 		
 	},
 	
 	render: function() {
 		return (<div>
 				<form>
-					<input type="text" />
+					<textarea id="issue-input" />
 					<input type="submit" onClick={this.handleSubmit} />
 				</form>
 			</div>);
